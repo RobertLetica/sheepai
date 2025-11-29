a2a02a4fee485b8105de4f14e88bb4bffa96b12f
Robert Letica <robert.letica2203@gmail.com>
2025-11-29 12:24:06 +0100

    users.json

---
+++ src/users.js
@@ -0,0 +1,23 @@
+[
+    {
+        mail: "example@example.com",
+        otp: "123123",
+        token: "xyxy",
+        tags: [
+            {
+                "name": "Google Cloud",
+                "confidence": 0.8
+            },
+            {
+                "name": "Microsoft Azure",
+                "confidence": 0.8
+            },
+            {
+                "name": "AI",
+                "confidence": 0.9
+            },
+        ],
+        interests_prompt: "",
+        last_online: "",
+    }
+]
